8efd65e81cebf67fd430055a4f8faa0948d1a856
Millan Garcia <millangg17@gmail.com>
2020-11-17 11:50:24 +0100

    playing with props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Saludar from "./components/Saludar";
 
 function App() {
+  const user = {
+    name: "Millan Garcia",
+    age: 25,
+    hobby: "cooking",
+  };
+
+  const saludarFn = () => {
+    console.log("hola");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Saludar userinfo={user} saludarFn={saludarFn} />
       </header>
     </div>
   );
---
+++ src/components/Saludar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Saludar(props) {
+  console.log(props);
+  return (
+    <div>
+      <button onClick={props.saludarFn}>Saludar</button>
+    </div>
+  );
+}
